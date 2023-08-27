52fd97bb760b192f861c171b7b9136a5f93d06de
huerta-federico <65100237+huerta-federico@users.noreply.github.com>
2023-08-27 13:37:46 -0500

    test 2

--- internas/config.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    //Parámetros para la conexión a la BD
-    define('DBHOST', 'localhost');
-    define('DBUSER', 'root');
-    define('DBPASS', '');
-    define('DBNAME', 'acdb2_02bim');
-?>
