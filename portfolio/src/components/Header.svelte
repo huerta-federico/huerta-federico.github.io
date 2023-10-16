1f3afe3d661f332283890b2cbccc63bb06046456
huerta-federico <65100237+huerta-federico@users.noreply.github.com>
2023-10-15 21:43:47 -0500

    initial

---
+++ portfolio/src/components/Header.svelte
@@ -0,0 +1,42 @@
+<script>
+    export let y;
+    
+    // Top bar navigation links
+    export let tabs = [
+        { name: "Projects", link: "#projects" },
+        { name: "About me", link: "#about" },
+        { name: "Extra", link: "#extra" },
+    ];
+</script>
+
+<header
+    class={"sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border-b border-solid " +
+        (y > 0
+            ? " py-4 bg-slate-950 border-violet-950"
+            : " py-6 bg-transparent border-transparent")}
+>
+    <h1 class="font-medium">
+        <b class="font-bold poppins">Federico</b> Huerta
+    </h1>
+    <div class="sm:flex items-center gap-4 hidden">
+        {#each tabs as tab, index}
+            <a
+                href={tab.link}
+                class="duration-200 hover:text-violet-400"
+                target={index == 2 ? "_blank" : ""}
+            >
+                <p>{tab.name}</p>
+            </a>
+        {/each}
+        <a
+            class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950"
+            href="a"
+            target="_blank"
+        >
+            <div
+                class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+            />
+            <h4 class="relative z-9">Get in touch &rarr;</h4>
+        </a>
+    </div>
+</header>
---
+++ portfolio/src/components/Main.svelte
@@ -0,0 +1,242 @@
+<script>
+    import Step from "./Step.svelte";
+
+    // Projects Array
+    let steps = [
+        {
+            name: "Project 1",
+            icon: "fa-brands fa-linux",
+            description: "project description 1",
+        },
+        {
+            name: "Project 2",
+            icon: "fa-brands fa-linux",
+            description: "project description 2",
+        },
+        {
+            name: "Project 3",
+            icon: "fa-brands fa-linux",
+            description: "project description 3",
+        },
+    ];
+
+    // Benefits Array
+    let benefits = [
+        {
+            name: "Benefit 1",
+            icon: "fa-brands fa-linux",
+            description: "the best DPS in PC NA",
+        },
+        {
+            name: "Benefit 2",
+            icon: "fa-brands fa-linux",
+            description: "an excelent cookie giver",
+        },
+        {
+            name: "Benefit 3",
+            icon: "fa-brands fa-linux",
+            description: "a great passive listener",
+        },
+    ];
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+    <!-- Intro section -->
+    <section
+        id="introPage"
+        class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14"
+    >
+        <div
+            class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10"
+        >
+            <!-- Section title -->
+            <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+                Hi! I'm <span class="poppins text-violet-400">Federico</span>
+                Huerta
+                <br />IT Engineering
+                <span class="poppins text-violet-400">Student</span>
+            </h2>
+
+            <!-- Section subtitle -->
+            <p class="text-base sm:text-lg md:text-xl">
+                My <span class="text-violet-400"> favorite stuff</span> includes
+                things, cookies, more stuff, bits and pieces, and you guessed it,
+                EVEN MOAR STUFF!
+            </p>
+
+            <!-- Get in Touch button-->
+            <a
+                class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950"
+                href="a"
+                target="_blank"
+            >
+                <div
+                    class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"
+                />
+                <h4 class="relative z-9">Get in touch &rarr;</h4>
+            </a>
+        </div>
+        <div class="relative shadow-2xl grid place-items-center">
+            <img
+                src={"images/profile.png"}
+                alt="Zetane Engine"
+                class="object-cover z-[2] max-h-[70vh]"
+            />
+        </div>
+    </section>
+
+    <!-- Project section -->
+    <section id="projects" class="py-20 lg:py-32 flex flex-col gap-24">
+        <div class="flex flex-col gap-2 text-center">
+            <h6 class="text-large sm:text-xl md:textxl">A few of my stuff</h6>
+            <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+                Curious to <span class="poppins text-violet-400">see</span> my stuff?
+            </h3>
+        </div>
+        <a
+            href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+            target="_blank"
+            class="mx-auto px-4 py-2 rounded-md border border-solid border-white flex items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-violet-700 duration-200"
+        >
+            <i class="fa-regular fa-circle-play" />
+            <p>Watch the video</p>
+        </a>
+
+        <!-- Project containers -->
+        <div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
+            <Step step={steps[0]}>
+                <p>
+                    Project 1 is just a placeholder for the description of
+                    stuff, things and <strong class="text-violet-400"
+                        >specially the super important stuff</strong
+                    > with some added detail at the end.
+                </p>
+            </Step>
+            <Step step={steps[1]}>
+                <p>
+                    Project 2 is just a placeholder for the description of
+                    stuff, things and <strong class="text-violet-400"
+                        >specially the super important stuff</strong
+                    > with some added detail at the end.
+                </p></Step
+            >
+            <Step step={steps[2]}>
+                <p>
+                    Project 3 is just a placeholder for the description of
+                    stuff, things and <strong class="text-violet-400"
+                        >specially the super important stuff</strong
+                    > with some added detail at the end.
+                </p></Step
+            >
+        </div>
+    </section>
+
+    <!-- About section -->
+    <section
+        id="about"
+        class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative"
+    >
+        <div
+            class="flex flex-col gap-2 text-center relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700 after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4"
+        >
+            <h6 class="text-lg sm:text-xl md:text-2xl">Want to know more?</h6>
+            <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+                A bit <span class="poppins text-violet-400">about</span> me.
+            </h3>
+        </div>
+        <p class="mx-auto poppins font-semibold text-lg sm:text-xl md:text-2xl">
+            I am . . .
+        </p>
+
+        <!-- Benefits containers -->
+        <div class="flex flex-col gap-20 w-full mx-auto max-w-[800px]">
+            {#each benefits as benefit, index}
+                <div class="flex gap-6 sm:gap-8">
+                    <p
+                        class="poppins text-4xl sm:text-5xl md:text-6xl text-slate-500 font-semibold"
+                    >
+                        0{index + 1}
+                    </p>
+                    <div class="flex flex-col gap-6 sm:gap-8">
+                        <h3 class="text-2xl sm:text-3xl md:text-5xl">
+                            {benefit.name}
+                        </h3>
+                        <p>{benefit.description}</p>
+                    </div>
+                </div>
+            {/each}
+        </div>
+
+        <!-- Table of goodies -->
+        <h5 class={" text-2xl sm:text-3xl font-semibold text-center poppins "}>
+            The <span class="text-violet-400">Complete</span> Package
+        </h5>
+        <div
+            class="flex flex-col overflow-x-scroll gap-10 max-w-[800px] mx-auto w-full"
+        >
+            <table class="bg-white text-slate-700 rounded text-center">
+                <thead class={"border-b border-solid border-slate-200  "}>
+                    <tr class="">
+                        <th />
+                        <th class="whitespace-nowrap p-2 px-4">One dude</th>
+                        <th class="whitespace-nowrap p-2 px-4">Another dude</th>
+                        <th class="whitespace-nowrap p-2 px-4">The Last dude</th
+                        >
+                        <th
+                            class="bg-violet-700 text-white whitespace-nowrap p-4 px-8"
+                            >Me</th
+                        >
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="border-b border-solid border-slate-200">
+                        <td
+                            class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
+                            >Dedication</td
+                        >
+                        <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-slate-500" /></td>
+                        <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-green-500" /></td>
+                    </tr>
+                    <tr class="border-b border-solid border-slate-200">
+                        <td
+                            class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
+                            >Critical Thought</td
+                        >
+                        <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-green-500" /></td>
+                    </tr>
+                    <tr>
+                        <td
+                            class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
+                            >Interpersonal Skills</td
+                        >
+                        <td><i class="fa-solid fa-check text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-slate-500" /></td>
+                        <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-green-500" /></td>
+                    </tr>
+                    <tr class="border-t border-solid border-slate-200">
+                        <td
+                            class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm"
+                            >Progamming Ability</td
+                        >
+                        <td><i class="fa-solid fa-check text-slate-500" /></td>
+                        <td><i class="fa-solid fa-xmark text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-slate-500" /></td>
+                        <td><i class="fa-solid fa-check text-green-500" /></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+
+        <!-- Table hint for overflow -->
+        <div class="mx-auto -mt-12 italic sm:hidden opacity-50">
+            <p>Scroll to see more &rarr;</p>
+        </div>
+        <p class="mx-auto">So why not do stuff with me?</p>
+    </section>
+</main>
