addd9b66f136eee316f275c9bbaa1e7fe8f75161
huerta-federico <65100237+huerta-federico@users.noreply.github.com>
2023-10-15 22:57:28 -0500

    test for gh action

--- portfolio/src/components/Main.svelte
+++ portfolio/src/components/Main.svelte
@@ -52,7 +52,7 @@
             <!-- Section title -->
             <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
                 Hi! I'm <span class="poppins text-violet-400">Federico</span>
-                Huerta
+                Huerta!!!
                 <br />IT Engineering
                 <span class="poppins text-violet-400">Student</span>
             </h2>
